e46b76f1f4b64e479e117e509c82444f8c6b1281
evanallen13 <evan.allen@xebia.com>
2025-03-19 20:28:07 +0000

    add to gitignore

--- main.py
+++ main.py
@@ -22,11 +22,9 @@ def main():
     print("Adding documents to database...")
     db = database("database")
 
-    print("Add first 5000 documents to database...")
-    db.add_to_chroma(sliced_documents[1:5000])
-    print("Add last 5000 documents to database...")
-    db.add_to_chroma(sliced_documents[5000:10000])
-    
+    print("Add first 2000 documents to database...")
+    db.add_to_chroma(sliced_documents[1:50])
+
     llm = llama(OLLAMA_HOST, MODEL)
 
 if __name__ == "__main__":
