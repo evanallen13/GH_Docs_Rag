1ac8b2f7ccd3cef1fa52a622ed70e2bb1b62eadc
evanallen13 <evan.allen@xebia.com>
2025-03-19 13:59:46 -0400

    add chroma

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from main import main
+
+app = Flask(__name__)
+main()
+
+@app.route('/')
+def home():
+    return "Welcome to my Flask app!"
+
+@app.route('/api/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'Guest')
+    return jsonify({"message": f"Hello, {name}!"})
+
+@app.route('/api/data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    return jsonify({"received": data}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from src.git_helper import git_helper
 from src.document_loader import document_loader # type: ignore
+from src.database import database
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import os
+from src.llama import llama
+
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://ollama:11434")
+MODEL = "gemma3:1b" # Find available models here https://ollama.com/library
+
+def main():
+    print("Cloning or updating GitHub repository...")
+    # git_helper() # clone or update the GitHub repository
+    
+    print("Loading documents...")
+    doc_loader = document_loader()
+    documents = doc_loader.load_documents()
+    sliced_documents = doc_loader.split_documents(documents) 
+    
+    print("Adding documents to database...")
+    db = database("database")
+
+    print("Add first 5000 documents to database...")
+    db.add_to_chroma(sliced_documents[1:5000])
+    print("Add last 5000 documents to database...")
+    db.add_to_chroma(sliced_documents[5000:10000])
+    
+    llm = llama(OLLAMA_HOST, MODEL)
 
 if __name__ == "__main__":
-    git_helper() # clone or update the GitHub repository
-    
-    document_loader = document_loader()
-    documents = document_loader.load_documents()
-    sliced_documents = document_loader.split_documents(documents) 
-    
-    print(len(documents))
+    main() 
