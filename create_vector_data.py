d9914cd8948639e7dc9f0a7d9818e90f3edde2c2
Evan Allen <evanallen13@gmail.com>
2025-03-20 14:30:21 +0000

    add embber

--- create_vector_data.py
+++ create_vector_data.py
@@ -1,18 +1,14 @@
 from src.document_loader import document_loader # type: ignore
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
-
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
-def get_embedding(text):
+def get_embedding(client, text):
     response = client.embeddings.create(
         input=text, model=EMBEDDING_MODEL
     )
@@ -25,13 +21,20 @@ def main():
     documents = doc_loader.load_documents()
     sliced_documents = doc_loader.split_documents(documents) 
     
-    
-    for doc in sliced_documents[0:10]:
-        embed=get_embedding("This is a test")
-        print(str(doc))
-        print("--------")
-        print(embed)
-        print("\n \n")
+    for doc in sliced_documents[0:1000]:
+        page_content = doc.page_content
+        metadata = doc.metadata
+        embedded=get_embedding(page_content)
+        
+        # embed=get_embedding(doc.page_content)
+        
+        # page_content = doc.page_content
+        # metadata = doc.metadata
+        
+        # print(str(doc))
+        # print("--------")
+        # print(embed)
+        # print("\n \n")
         
 
 if __name__ == "__main__":
