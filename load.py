7fdd54190819351178a3e83116da55688c4caa06
Evan Allen <evanallen13@gmail.com>
2025-03-28 18:36:13 +0000

    add query

---
+++ load.py
@@ -0,0 +1,59 @@
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+from src.document_loader import document_loader
+from src.git_helper import git_helper
+from langchain_openai import OpenAIEmbeddings
+import os
+import random
+from dotenv import load_dotenv
+load_dotenv()
+
+EMBEDDING_MODEL="text-embedding-ada-002"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+COSMOS_URL = os.getenv("COSMOS_URL")
+COSMOS_KEY = os.getenv("COSMOS_KEY")
+
+client = CosmosClient(COSMOS_URL, credential=COSMOS_KEY )
+database_name = "MyDatabase"
+container_name = "docs"
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+openai_embeddings = OpenAIEmbeddings(
+    model=EMBEDDING_MODEL,
+    openai_api_key=OPENAI_API_KEY
+)
+
+print("Cloning or updating GitHub repository...")
+git_helper()
+
+print("Loading documents...")
+doc_loader = document_loader()
+documents = doc_loader.load_documents()
+sliced_documents = doc_loader.split_documents(documents) 
+
+print(f"Adding {len(sliced_documents)} documents to Cosmos.")
+file_name = {}
+for doc in sliced_documents:
+    title = doc.metadata['source']
+    content = doc.page_content
+    embed = openai_embeddings.embed_query(title + " " + content)
+    page_num = None
+    
+    if title in file_name:
+        page_num = file_name[title] + 1  
+    else:
+        page_num = 1
+        file_name[title] = page_num 
+        
+    document = {
+        "filename": title,
+        "page": page_num,
+        "content": content,
+        "vector": embed,
+        "id": str(random.randint(999999999, 9999999999)),
+    }
+    try:
+        container.create_item(body=document)
+        print(f"Book '{title}' added successfully.")
+    except exceptions.CosmosHttpResponseError as e:
+        print(f"Failed to add book '{title}': {e.message}")
---
+++ query.py
@@ -0,0 +1,43 @@
+import os
+import json
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
+
+EMBEDDING_MODEL="text-embedding-ada-002"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+COSMOS_URL = os.getenv("COSMOS_URL")
+COSMOS_KEY = os.getenv("COSMOS_KEY")
+
+client = CosmosClient(COSMOS_URL, credential=COSMOS_KEY )
+database_name = "MyDatabase"
+container_name = "docs"
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+openai_embeddings = OpenAIEmbeddings(
+    model=EMBEDDING_MODEL,
+    openai_api_key=OPENAI_API_KEY
+)
+
+query_string = "How much does Copilot Enterprise cost?"
+query_embedding = openai_embeddings.embed_query(query_string)
+
+k_num = 3
+query = f'SELECT TOP {k_num} c.id, c.filename, VectorDistance(c.vector, @embedding) AS SimilarityScore FROM c ORDER BY VectorDistance(c.vector, @embedding)'
+parameters = [{"name": "@embedding", "value": query_embedding}]
+
+query_iterable = container.query_items(
+    query=query,
+    parameters=parameters,
+    enable_cross_partition_query=True,
+    populate_index_metrics=True,
+    populate_query_metrics=True
+)
+
+items = list(query_iterable)
+for item in items:
+    print(json.dumps(item, indent=2))
+    
+print("Request Units consumed: ",container.client_connection.last_response_headers['x-ms-request-charge'])
